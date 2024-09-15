bee93ddb9ccbf8e68e156ccd8990980ea87ad2f5
Dhruvish Domadiya <ddc0146@gmail.com>
2024-09-15 22:05:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import HeroSelection from './components/HeroSelection'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <Navbar/>
+        Hello World !!!!
+        stash line 3
+        this is clone repo by dhruvish
+        <HeroSelection/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <img src="react.svg" alt="logo"/>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
